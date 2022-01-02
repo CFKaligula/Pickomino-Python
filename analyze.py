f4e89c9b937997081508c87fe105adb498d058b5
C.F.K <casper.kole@gmail.com>
2022-01-02 12:09:29 +0100

    rename regenwormen to picomino

--- analyze.py
+++ analyze.py
@@ -4,40 +4,42 @@ import os
 
 folder_path = os.path.dirname(__file__)
 
+
 def histogram_analysis():
-    print(os.path.join(folder_path,"analysis.txt"))
-    with open(os.path.join(folder_path,"analysis.txt"), "r") as analysis_file:
+    print(os.path.join(folder_path, "analysis.txt"))
+    with open(os.path.join(folder_path, "analysis.txt"), "r") as analysis_file:
         lines = analysis_file.readlines()
 
     data = []
     for line in lines:
         number_of_wins = re.search(r"([0-9]*) w", line)
         data.append(int(number_of_wins.group(1)))
-    print("data",data)
+    print("data", data)
 
     count_data = []
     for i in range(len(data)):
         count_data.extend([i]*int(data[i]))
-    print(count_data)  
+    print(count_data)
 
     plt.style.use('ggplot')
-    plt.hist(count_data, bins = 100)
+    plt.hist(count_data, bins=100)
     plt.show()
-    
+
+
 def run_multiple_times():
     let_analysis_file_path = os.path.join(folder_path, "let_analysis.txt")
     with open(let_analysis_file_path, "w") as analysis_file:
         analysis_file.write("")
     for i in range(1000):
         print(i)
-        os.system(f"python {os.path.join(folder_path, 'regenwormen.py')}") 
-        
+        os.system(f"python {os.path.join(folder_path, 'picomino.py')}")
+
     with open(let_analysis_file_path, "r") as analysis_file:
         lines = analysis_file.readlines()
     data = []
     for line in lines:
         data.append(line.strip())
-    print("data",data)
+    print("data", data)
     count_dict = {}
     for d in data:
         if d in count_dict:
@@ -48,6 +50,6 @@ def run_multiple_times():
     plt.style.use('ggplot')
     plt.hist(data)
     plt.show()
-    
-    
+
+
 histogram_analysis()
--- player_strategy_test.py
+++ player_strategy_test.py
@@ -1,6 +1,6 @@
 import globals
 import player_strategies
-from regenwormen import roll_dice, let_player_roll
+from picomino import roll_dice, let_player_roll
 from random import randint, choices
 import visualize
 import os
@@ -9,25 +9,26 @@ _NUMBER_OF_DICE_ROLLS = 1000
 player_strategy = player_strategies.smart_player("player")
 for i in range(_NUMBER_OF_DICE_ROLLS):
     globals.active_tiles = list(range(21, 37))
-    
-    rollable_dice_number = randint(1,8)
+
+    rollable_dice_number = randint(1, 8)
     player_strategy.dice_in_hand = choices(globals._DICE_OPTIONS, k=globals._NUMBER_OF_DICE - rollable_dice_number)
-    
+
     roll_stop_decision = player_strategy.decide_roll_or_stop()
-    if globals.verbose: print("roll or stop decision:", roll_stop_decision)
+    if globals.verbose:
+        print("roll or stop decision:", roll_stop_decision)
 
     # player decides to roll
     if roll_stop_decision == "roll":
-       player_turn_over, rollable_dice_number = let_player_roll(rollable_dice_number, player_strategy)
-       
+        player_turn_over, rollable_dice_number = let_player_roll(rollable_dice_number, player_strategy)
+
     # player has chosen to stop
     else:
-        if globals.verbose: print("Player decided to stop")
-        if globals.verbose: 
+        if globals.verbose:
+            print("Player decided to stop")
+        if globals.verbose:
             print("dice in hand:")
             visualize.visualize_dice(player_strategy.dice_in_hand)
 
     if globals.verbose:
         input()
         os.system("cls||clear")
-    
